869c00b3c776e32a8bc8c331a4ba5bc2308e9be4
TechRami <techrami@outlook.com>
2023-07-19 20:52:12 +0300

    init car customization menu and name car parts

--- src/components/Bugatti.tsx
+++ src/components/Bugatti.tsx
@@ -66,6 +66,8 @@ type GLTFResult = GLTF & {
 
 export function Bugatti(props: JSX.IntrinsicElements["group"]) {
   const { nodes, materials } = useGLTF("/models/bugatti.glb") as GLTFResult;
+  const bodyColor = new THREE.MeshStandardMaterial({ color: "red" });
+
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
@@ -73,106 +75,132 @@ export function Bugatti(props: JSX.IntrinsicElements["group"]) {
           geometry={nodes.Object_2.geometry}
           material={materials.Matte__80800000__env_50_spec_trans}
         />
+        {/* not this one below */}
         <mesh
           geometry={nodes.Object_3.geometry}
           material={materials.lavoiturecsr2_badge__env_50_spec}
         />
+        {/* exhaust color */}
         <mesh
           geometry={nodes.Object_4.geometry}
           material={materials.lavoiturecsr2_grille1__env_50_spec}
         />
+        {/* car bottom */}
         <mesh
           geometry={nodes.Object_5.geometry}
           material={materials.lavoiturecsr2_grille2__env_50_spec}
         />
+        {/* dont even bother */}
         <mesh
           geometry={nodes.Object_6.geometry}
           material={materials.lavoiturecsr2_grille3__env_50_spec}
         />
+        {/* left light */}
         <mesh
           geometry={nodes.Object_7.geometry}
           material={materials.vehiclelights128__env_50_spec_FL}
         />
+        {/* right light */}
         <mesh
           geometry={nodes.Object_8.geometry}
           material={materials.vehiclelights128__env_50_spec_FR}
         />
+        {/* rear light */}
         <mesh
           geometry={nodes.Object_9.geometry}
           material={materials.vehiclelights128__env_50_spec_RR}
         />
+        {/* every thing with glass material */}
         <mesh
           geometry={nodes.Object_10.geometry}
           material={materials.Matte__80202020__env_50_spec_trans}
         />
+        {/* car skirt */}
         <mesh
           geometry={nodes.Object_11.geometry}
           material={materials.Matte__FF114182__sec_env_50_spec}
         />
+        {/* inside body obv */}
         <mesh
           geometry={nodes.Object_12.geometry}
           material={materials.Matte__FF151515__spec}
         />
+        {/* whole body except doors and skirt */}
         <mesh
           geometry={nodes.Object_13.geometry}
           material={materials.Matte__FFFFFFFF__prim_env_50_spec}
         />
+        {/* vechile doors */}
         <mesh
           geometry={nodes.Object_14.geometry}
           material={materials.Matte__FFFFFFFF__prim_env_50_spec}
         />
+        {/* add to customization menu. car lines and front */}
         <mesh
           geometry={nodes.Object_15.geometry}
           material={materials.lavoiturecsr2_coloured__env_50_spec}
         />
+        {/* car rear */}
         <mesh
           geometry={nodes.Object_16.geometry}
           material={materials.lavoiturecsr2_coloured__env_50_spec}
         />
+        {/* mirrors */}
         <mesh
           geometry={nodes.Object_17.geometry}
           material={materials.lavoiturecsr2_coloured__env_50_spec}
         />
+        {/* car rear lights and text */}
         <mesh
           geometry={nodes.Object_18.geometry}
           material={materials.lavoiturecsr2_light__env_50_spec}
         />
+        {/* inside colors like chair etc */}
         <mesh
           geometry={nodes.Object_19.geometry}
           material={materials.lavoiturecsr2_textured2a__spec}
         />
+        {/* inside doors */}
         <mesh
           geometry={nodes.Object_20.geometry}
           material={materials.lavoiturecsr2_textured2a__spec}
         />
+        {/* car exhauts */}
         <mesh
           geometry={nodes.Object_21.geometry}
           material={materials.lavoiturecsr2_textureda__env_50_spec}
         />
+        {/* car front seperated */}
         <mesh
           geometry={nodes.Object_22.geometry}
           material={materials.lavoiturecsr2_textureda__env_50_spec}
         />
+        {/* wheeles */}
         <mesh
           geometry={nodes.Object_23.geometry}
           material={materials.lavoiturecsr2_wheel__env_50_spec}
         />
+        {/* car tires */}
         <mesh
           geometry={nodes.Object_24.geometry}
           material={materials.lavoiturecsr2_wheel__spec}
         />
+        {/* wheel rear thing */}
         <mesh
           geometry={nodes.Object_25.geometry}
           material={materials.lavoiturecsr2_wheeltextured__env_50_spec}
         />
+        {/* wheel rear thing */}
         <mesh
           geometry={nodes.Object_26.geometry}
           material={materials.lavoiturecsr2_wheeltextured__env_50_spec}
         />
+        {/* wheel rear thing */}
         <mesh
           geometry={nodes.Object_27.geometry}
           material={materials.lavoiturecsr2_wheeltextured__env_50_spec}
         />
+        {/* wheel rear thing */}
         <mesh
           geometry={nodes.Object_28.geometry}
           material={materials.lavoiturecsr2_wheeltextured__env_50_spec}
--- src/components/ui/menus/CarCustomizationMenu.tsx
+++ src/components/ui/menus/CarCustomizationMenu.tsx
@@ -1,7 +1,22 @@
 import Menu from "../Menu";
 
 const CarCustomizationMenu = () => {
-  return <Menu>hello world</Menu>;
+  return (
+    <Menu>
+      <div className="flex flex-col">
+        <h3 className="text-white font-medium capitalize">Body Color:</h3>
+        <span className="h-10 w-10 block bg-red-400 rounded-full" />
+      </div>
+      <div className="flex flex-col">
+        <h3 className="text-white font-medium capitalize">wheel Color:</h3>
+        <span className="h-10 w-10 block bg-red-400 rounded-full" />
+      </div>
+      <div className="flex flex-col">
+        <h3 className="text-white font-medium capitalize">Mirror Color:</h3>
+        <span className="h-10 w-10 block bg-red-400 rounded-full" />
+      </div>
+    </Menu>
+  );
 };
 
 export default CarCustomizationMenu;
