69f2604e676e8e0088ee81e9dac41e81a43d3c49
TechRami <techrami@outlook.com>
2023-07-20 05:02:17 +0300

    add reflection to material

--- src/components/ui/menus/CarCustomizationMenu.tsx
+++ src/components/ui/menus/CarCustomizationMenu.tsx
@@ -2,6 +2,8 @@ import { useCustomizationStore } from "../../../store/customization";
 import Menu from "../Menu";
 
 const colorOptions = [
+  { name: "default", value: null, tw: "bg-white" },
+  { name: "black", value: "#000000", tw: "bg-neutral-800" },
   { name: "red", value: "#ff0000", tw: "bg-red-500" },
   { name: "blue", value: "#0000ff", tw: "bg-blue-500" },
   { name: "green", value: "#00ff00", tw: "bg-green-500" },
@@ -10,7 +12,7 @@ const colorOptions = [
 const CarCustomizationMenu = () => {
   const { setBodyColor } = useCustomizationStore(); // Get bodyColor and setBodyColor from your store
 
-  const handleColorSelection = (colorValue: string) => {
+  const handleColorSelection = (colorValue: string | null) => {
     setBodyColor(colorValue);
   };
 
