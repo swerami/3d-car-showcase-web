e80ddcd2005f08e62cd9e8d72acaeec012973434
TechRami <techrami@outlook.com>
2023-07-20 06:01:54 +0300

    refactoring

--- src/components/ui/menus/CarCustomizationMenu.tsx
+++ src/components/ui/menus/CarCustomizationMenu.tsx
@@ -1,7 +1,14 @@
+import React from "react";
 import { useCustomizationStore } from "../../../store/customization";
 import Menu from "../Menu";
 
-const colorOptions = [
+interface ColorOption {
+  name: string;
+  value: string | null;
+  tw: string;
+}
+
+const colorOptions: ColorOption[] = [
   { name: "default", value: null, tw: "bg-white" },
   { name: "black", value: "#000000", tw: "bg-neutral-800" },
   { name: "red", value: "#ff0000", tw: "bg-red-500" },
@@ -9,25 +16,76 @@ const colorOptions = [
   { name: "green", value: "#00ff00", tw: "bg-green-500" },
 ];
 
-const CarCustomizationMenu = () => {
-  const { setBodyColor } = useCustomizationStore();
+interface ColorButtonProps {
+  colorOption: ColorOption;
+  colorSetter: (colorValue: string | null) => void;
+}
 
-  const handleColorSelection = (colorValue: string | null) => {
-    setBodyColor(colorValue);
-  };
+const CarCustomizationMenu: React.FC = () => {
+  const { setBodyColor, setHubcapColor, setBodySecondaryColor } =
+    useCustomizationStore();
+
+  const setColor =
+    (colorSetter: (colorValue: string | null) => void) =>
+    (colorValue: string | null) => {
+      colorSetter(colorValue);
+    };
+
+  const ColorButton: React.FC<ColorButtonProps> = ({
+    colorOption,
+    colorSetter,
+  }) => (
+    <button
+      key={colorOption.name}
+      className={`h-10 w-10 rounded-xl ${colorOption.tw} transition-transform transform focus:animate-wiggle`}
+      onClick={() => setColor(colorSetter)(colorOption.value)}
+    />
+  );
 
   return (
     <Menu>
-      <div className="flex flex-col">
-        <h3 className="text-white font-medium capitalize">Body Color:</h3>
-        <div className="flex space-x-2">
-          {colorOptions.map((option) => (
-            <button
-              key={option.name}
-              className={`h-10 w-10 rounded-full ${option.tw} transition-transform transform focus:animate-wiggle`}
-              onClick={() => handleColorSelection(option.value)}
-            />
-          ))}
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col">
+          <h3 className="text-white text-xl pb-2 font-medium capitalize">
+            Body Color:
+          </h3>
+          <div className="flex space-x-2">
+            {colorOptions.map((option) => (
+              <ColorButton
+                key={option.name}
+                colorOption={option}
+                colorSetter={setBodyColor}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <h3 className="text-white text-xl pb-2 font-medium capitalize">
+            Body Secondary Color:
+          </h3>
+          <div className="flex space-x-2">
+            {colorOptions.map((option) => (
+              <ColorButton
+                key={option.name}
+                colorOption={option}
+                colorSetter={setBodySecondaryColor}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <h3 className="text-white text-xl pb-2 font-medium capitalize">
+            Hubcap Color:
+          </h3>
+          <div className="flex space-x-2">
+            {colorOptions.map((option) => (
+              <ColorButton
+                key={option.name}
+                colorOption={option}
+                colorSetter={setHubcapColor}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </Menu>
