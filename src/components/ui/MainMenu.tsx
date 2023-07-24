898f570933d7daa89d080ce7700a716b107dcb29
TechRami <techrami@outlook.com>
2023-07-24 23:49:34 +0300

    fixing camera change on color selection

--- src/components/ui/MainMenu.tsx
+++ src/components/ui/MainMenu.tsx
@@ -5,13 +5,13 @@ import useNavigationStore from "../../store/navigation";
 import Menu from "./Menu";
 import PerformanceMenu from "./menus/PerformanceMenu";
 import CarCustomizationMenu from "./menus/CarCustomizationMenu";
-import useSettingsStore from "../../store/settings";
+import useSettingsStore, { ViewMode } from "../../store/settings";
 import { useFrame } from "@react-three/fiber";
 import { gsap } from "gsap";
 
 const MainMenu = () => {
   const { activeMenu } = useNavigationStore();
-  const { camera, hubcapViewMode, wheelCam } = useSettingsStore();
+  const { camera, viewMode, wheelCam, originalPosition } = useSettingsStore();
   const currentActiveMenu = () => {
     switch (activeMenu) {
       case "performance":
@@ -22,10 +22,43 @@ const MainMenu = () => {
         return <Menu />;
     }
   };
+
   useFrame((state, delta) => {
-    if (hubcapViewMode) {
-      gsap.to(state.camera.position, { ...wheelCam.position, duration: 1 });
+    // create a switch case function to do mulitple different functions depending on various cases
+    switch (viewMode) {
+      case ViewMode.ActiveBodyViewmode:
+        gsap.to(state.camera.position, {
+          x: originalPosition.x,
+          y: originalPosition.y,
+          z: originalPosition.z,
+          duration: 1,
+        });
+        state.camera.clear();
+        break;
+      case ViewMode.HubcapViewMode:
+        gsap.to(state.camera.position, { ...wheelCam.position, duration: 1 });
+        console.log("hi");
+        state.camera.clear();
+        state.camera.clearViewOffset();
+        state.camera.updateProjectionMatrix();
+        break;
+      default:
+        // state.camera.position.set(3, 2, -5);
+        state.camera.clear();
+        state.camera.clearViewOffset();
+        break;
     }
+
+    // if (viewMode) {
+    //   gsap.to(state.camera.position, { ...wheelCam.position, duration: 1 });
+    // } else if (viewMode) {
+    //   gsap.to(state.camera.position, {
+    //     x: originalPosition.x,
+    //     y: originalPosition.y,
+    //     z: originalPosition.z,
+    //     duration: 1,
+    //   });
+    // }
   });
 
   return (
